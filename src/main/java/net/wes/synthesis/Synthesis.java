cf37ef7770078c72f8a838c9cc9c876eaa67b2ee
wsgonc <wsgonc@gmail.com>
2026-03-28 21:39:05 -0400

    first commit

---
+++ src/main/java/net/wes/synthesis/Synthesis.java
@@ -0,0 +1,16 @@
+package net.wes.synthesis;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Synthesis implements ModInitializer {
+	public static final String MOD_ID = "synthesis";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/wes/synthesis/SynthesisClient.java
@@ -0,0 +1,10 @@
+package net.wes.synthesis;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SynthesisClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
